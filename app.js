96c5e6eeefd112cea6741ca34987c7647d3e69d4
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-04 15:09:55 +0300

    Add books route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const require = require('morgan');
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('tiny'));
+
+
+const bookRouter = express.Router();
+bookRouter.route('/books')
+    .get((req, res) => {
+        const response = { welcome: "Hello welcome to the API" };
+        res.json(response);
+    });
 app.get('/', (req, res) => {
-    res.send("Welcome to my API");
+    res.send("Welcome to my API Home page");
 });
 
 const port = process.env.PORT || 3000;
