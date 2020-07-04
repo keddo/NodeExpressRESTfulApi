a31b5e8d5aa9c054340151dbe8d76815814156b0
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-04 14:10:18 +0300

    Add simple get request interface

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send("Welcome to my API");
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`App starting at: http://localhost:${port}`);
