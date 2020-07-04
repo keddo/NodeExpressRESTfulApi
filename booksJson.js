9eda02fd615278da32d80490482188e43fda2f34
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-04 15:51:25 +0300

    Add list of books to be imported to the database.

---
+++ booksJson.js
@@ -0,0 +1,50 @@
+db.books.insert([
+    {
+        title: "War and Peace",
+        genre: "Historical Fiction",
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    },
+    {
+        title: "les Miserables",
+        genre: "Historical Fiction",
+        author: 'Victor Hugo',
+        read: false
+    },
+    {
+        title: "A Journey into the center of the Earth.",
+        genre: "Science Fiction",
+        author: 'Jules Verne',
+        read: false
+    },
+    {
+        title: "The Dark World",
+        genre: "Fantasy",
+        author: 'Henry Kuttner',
+        read: false
+    },
+    {
+        title: "The Time Machine",
+        genre: "Science Fiction",
+        author: 'H. G. Wells',
+        read: false
+    },
+    {
+        title: "The Wind in the Willows",
+        genre: "Fantasy",
+        author: 'Kenneth Grahame',
+        read: false
+    },
+    {
+        title: "Life on the Mississippi",
+        genre: "History",
+        author: 'Kenneth Grahame',
+        read: false
+    },
+    {
+        title: "Life on the Mississippi",
+        genre: "History",
+        author: 'Kenneth Grahame',
+        read: false
+    }
+]);
