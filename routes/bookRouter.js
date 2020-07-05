908148c2bb8ddf13dc39523234d50c7569d26ed6
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-05 10:00:12 +0300

    Implementing patch request

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -28,7 +28,8 @@ function routes(Books) {
                 return res.send(err);
             }
             if (book) {
-                req.body = book;
+                // console.log(req.body);
+                req.book = book;
                 return next();
             }
             return res.sendStatus(404);
@@ -42,8 +43,24 @@ function routes(Books) {
             book.author = req.body.author;
             book.genre = req.body.genre;
             book.read = req.body.read;
-            book.save();
-            return res.send(book);
+            req.book.save((err) => {
+                if (err) return res.send(err);
+                return res.json(book);
+            });
+        })
+        .patch((req, res) => {
+            const { book } = req;
+            if (req.body._id) delete req.body._id;
+            Object.entries(req.body).forEach((item) => {
+                const key = item[0];
+                const value = item[1];
+                book[key] = value;
+            });
+            req.book.save((err) => {
+                if (err)
+                    return res.send(err);
+                return res.json(book);
+            });
         });
     return bookRouter;
 }
