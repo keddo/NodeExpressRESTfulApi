69ec408989dc956fc47a259eff8e8af0e6eed4b3
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-05 11:36:44 +0300

    HATEOAS: added hyperlink to filter book items

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -20,7 +20,14 @@ function routes(Books) {
         })
     });
     bookRouter.route('/books/:bookId')
-        .get((req, res) => res.json(req.book))
+        .get((req, res) => {
+            const returnBook = req.book.toJSON();
+            const genre = req.book.genre.replace(' ', '%20');
+            returnBook.links = {};
+            returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+
+            res.json(returnBook);
+        })
         .put((req, res) => {
             const { book } = req;
             book.title = req.body.title;
